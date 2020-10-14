8b73ea1bbac465c91956a9269e6daf09b2591d44
Nadine Delaire <ndelaire16@gmail.com>
2020-10-14 13:16:42 -0700

    added updates to all files

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     const apiKey = "80fe851452521571df17e0e7c488da43";
 
     $("#citysearch").on("click", function () {
@@ -6,9 +6,9 @@ $(document).ready(function() {
         console.log(cityName);
         currentWeather(cityName);
     });
-  
+
     function currentWeather(city) {
-       console.log("currentWeather")
+        console.log("currentWeather")
         let apiCitySearch = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
         console.log(apiCitySearch);
         $.ajax({
@@ -17,33 +17,64 @@ $(document).ready(function() {
         }).then(function (response) {
             console.log("query string");
             console.log(response);
-            uvIndex(response.coord.lat, response.coord.lon);
+             uvIndex(response.coord.lat, response.coord.lon);
+           
+
             fiveDay(city);
+            // let col = $("<div>").attr("class", "col")
+            let card = $("<div>").attr("class", "card")
+            let cardBody = $("<div>").attr("class", "card-body")
+            // add the extra things like wind speed, uv, etc duplicate line 29
+
             let title = $("<h1>").text(response.name);
-            $("#currentweather").append(title);
+            let currentCityWeather = $("<p>").text("Temperature: " + response.main.temp);
+            cardBody.append(title, currentCityWeather); //append each element
+            card.append(cardBody);
+            //col.append(card)
+            // $("#fivedayforecast").append(col);
+            $("#currentweather").append(card);
+            
         });
     }
-    function uvIndex (lat, lon){
+    
+    function uvIndex(lat, lon) {
         let queryUV = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
+        let result;
         $.ajax({
             url: queryUV,
             method: "GET"
-        }).then(function (response){
-            console.log (response);
+        }).then(function (response) {
+            console.log(response);
+            console.log("I am the value" + response.value);
+            let currentUvIndex = $("<button>").text(response.value);
+            if (response.value < 3) {
+                currentUvIndex.attr("class", "btn green");
+            } else if (currentUvIndex > 3 && currentUvIndex < 7) {
+                currentUvIndex.attr("class", "btn yellow")
+                
+            } else {
+                currentUvIndex.attr("class", "btn red");
+            }
+            
+            let uvIndexButton = $("<p>").text("UV Index: ").append(currentUvIndex);
+            $("#currentweather .card-body").append(uvIndexButton);
+            
         });
     }
-    function fiveDay (city) {
+
+    function fiveDay(city) {
         let forecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial";
         $.ajax({
             url: forecast,
             method: "GET"
-        }).then(function (response){
-            console.log (response);
-            const dailyData = response.list.filter(day => {   
+        }).then(function (response) {
+            console.log(response);
+            const dailyData = response.list.filter(day => {
                 return day.dt_txt.includes("15:00:00")
-                }
-              );
-              console.log(dailyData)
+            });
+            console.log(dailyData)
+            //use current weather as an example
+        //   do a for loop 
         });
     }
 
