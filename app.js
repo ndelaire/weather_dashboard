90693b6fb341f1d202a83a8ac156f508c7614e88
Nadine Delaire <ndelaire16@gmail.com>
2020-10-25 11:39:55 -0700

    adding to js

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 $(document).ready(function () {
     const apiKey = "80fe851452521571df17e0e7c488da43";
-
+    let searchHistory = JSON.parse(window.localStorage.getItem("city")) || [];
+    for (let i = 0; i < searchHistory.length; i++) {
+        searchButton(searchHistory[i])
+    }
     $("#citysearch").on("click", function () {
         let cityName = $("#city").val().trim();
         console.log(cityName);
         currentWeather(cityName);
+        if (searchHistory.indexOf(cityName)===-1) {
+            searchHistory.push(cityName);
+            window.localStorage.setItem("city", JSON.stringify(searchHistory));
+            searchButton(cityName);
+        }
     });
-// current city weather function
+    $("#citybuttons").on("click", "button", function(){
+        currentWeather($(this).val());
+    })
+    // current city weather function
     function currentWeather(city) {
         console.log("currentWeather")
         let apiCitySearch = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
@@ -17,23 +28,24 @@ $(document).ready(function () {
         }).then(function (response) {
             console.log("query string");
             console.log(response);
-             uvIndex(response.coord.lat, response.coord.lon);
-           fiveDay(city);
+            $("#currentweather").empty();
+            uvIndex(response.coord.lat, response.coord.lon);
+            fiveDay(city);
             let card = $("<div>").attr("class", "card")
             let cardBody = $("<div>").attr("class", "card-body")
             let title = $("<h1>").text(response.name);
             let currentCityWeather = $("<p>").text("Temperature: " + response.main.temp);
             let currentWindSpeed = $("<p>").text("Wind Speed: " + response.wind.speed);
             let currentHumidity = $("<p>").text("Humidity: " + response.main.humidity);
-            cardBody.append(title, currentCityWeather, currentHumidity, currentWindSpeed); 
+            cardBody.append(title, currentCityWeather, currentHumidity, currentWindSpeed);
             card.append(cardBody);
-            //col.append(card)
-            // $("#fivedayforecast").append(col);
+           
             $("#currentweather").append(card);
             
+
         });
     }
-    
+
     function uvIndex(lat, lon) {
         let queryUV = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
         let result;
@@ -48,40 +60,58 @@ $(document).ready(function () {
                 currentUvIndex.attr("class", "btn green");
             } else if (currentUvIndex > 3 && currentUvIndex < 7) {
                 currentUvIndex.attr("class", "btn yellow")
-                
+
             } else {
                 currentUvIndex.attr("class", "btn red");
             }
-            
+
             let uvIndexButton = $("<p>").text("UV Index: ").append(currentUvIndex);
             $("#currentweather .card-body").append(uvIndexButton);
-            
+
         });
     }
-// Five day forecast function 
+    // Five day forecast function 
     function fiveDay(city) {
         let forecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial";
-        let col = $("<div>").attr("class", "col")
-        let currentCityWeather = $("<p>").text("Temperature: " + response.main.temp);
-        let currentHumidity = $("<p>").text("Humidity: " + response.main.humidity);
+
+       
         $.ajax({
             url: forecast,
             method: "GET"
         }).then(function (response) {
             console.log(response);
+            $("#fivedayforecast").empty();
             const dailyData = response.list.filter(day => {
                 return day.dt_txt.includes("15:00:00")
             });
             console.log(dailyData)
-             col.append(card)
-            $("#fivedayforecast").append(col);
-            for (let i = 0; i < 5; i++){
-
+            
+            
+            for (let i = 0; i < dailyData.length; i++) {
+                let col = $("<div>").attr("class", "col");
+                let card = $("<div>").attr("class", "card");
+                let cardBody = $("<div>").attr("class", "card-body");
+                let date = $("<p>").text(new Date(dailyData[i].dt_txt).toLocaleDateString());
+                let currentCityWeather = $("<p>").text("Temperature: " + dailyData[i].main.temp);
+                let currentHumidity = $("<p>").text("Humidity: " + dailyData[i].main.humidity);
+                cardBody.append(date, currentCityWeather, currentHumidity);
+                card.append(cardBody);
+                col.append(card);
+                $("#fivedayforecast").append(col);
+    
             }
-             // cardBody.append(title, currentCityWeather, currentHumidity, currentWindSpeed); 
-            // card.append(cardBody);
+           
         });
+    
     }
 
+    function searchButton (cityName){
+        let listItem = $("<li>").attr("class", "list-group-item");
+        let cityButton = $("<button>").attr("class", "btn" );
+        cityButton.text(cityName).val(cityName);
+        // cityButton.onclick = currentWeather($(this.val()));
+        listItem.append(cityButton);
+        $("#citybuttons").append(listItem);
+    }
 
 });
